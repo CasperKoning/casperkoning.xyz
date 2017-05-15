2bc079fcf7656e7dc4a5166f124ac9e3f5b9e4a6
Casper Koning <casper.e.koning@gmail.com>
2017-05-15 23:19:55 +0200

    Setup templating with Handlebars

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 var express = require('express'),
-    parentApp = express();
+    app = express();
+var exphbs  = require('express-handlebars');
+var fs = require('fs');
+var path = require('path');
 
-parentApp.set('port', (process.env.PORT || 2368));
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+app.set('view engine', 'hbs');
 
-parentApp.use('/slides',express.static(__dirname + '/media/slides'))
+app.set('port', (process.env.PORT || 2368));
 
-parentApp.use('/', express.static(__dirname + '/app/public'))
+function getDirectories (srcpath) {
+  return fs.readdirSync(srcpath)
+    .filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
+}
 
-parentApp.listen(parentApp.get('port'), function() {
-  console.log('Node app is running on port', parentApp.get('port'));
+app.use('/slides', express.static(__dirname + '/media/slides'));
+
+app.get('/slides', function(req, res){
+  res.render('slides', {
+    slides: getDirectories('media/slides')
+  });
+});
+
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
